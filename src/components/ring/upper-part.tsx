fa53d6b5552c4734456e6d1362dab7c54c14517d
cysbyte <chenyunshu2@gmail.com>
2024-10-02 01:46:37 +0800

    fix: fixed bugs

--- src/components/ring/upper-part.tsx
+++ src/components/ring/upper-part.tsx
@@ -3,8 +3,8 @@ import upperPart from '/public/home/upper-part.svg'
 
 const UpperPart = () => {
   return (
-    <div className="w-full h-[600px] flex items-center justify-center bg-[url('/home/bg-ring.svg')] bg-center bg-no-repeat bg-cover">
-        <Image className='w-[700px] h-[500px] mb-6' src={upperPart} alt=''/>
+    <div className="w-full h-[800px] flex items-center justify-center bg-[url('/home/bg-ring.svg')] bg-center bg-no-repeat bg-cover">
+        <Image className='w-[750px] h-[600px] mb-6' src={upperPart} alt=''/>
     </div>
   )
 }
