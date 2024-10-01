9ce9485a708665ac6e3b2f050821d15285f100d9
cysbyte <chenyunshu2@gmail.com>
2024-10-02 01:18:49 +0800

    fix: fixed bugs

--- src/components/features/features.tsx
+++ src/components/features/features.tsx
@@ -10,7 +10,10 @@ const Features = () => {
                 <div className='w-full flex flex-col justify-center items-center gap-3'>
                     <h6 className='text-base text-[#FFAC9B]'>Features</h6>
                     <h3 className='text-5xl font-extrabold text-white'>Powerful iMerch AI Model</h3>
-                    <p className='text-sm text-white'>{`All of our services are powered by our cutting-edge 'iMerch AI', enabling<br /> a personalised solutions with unmatched speed and convenience.`}</p>
+                    <div className='text-center'>
+                    <p className='text-sm text-white'>{`All of our services are powered by our cutting-edge 'iMerch AI', enabling`}</p>
+                    <p className='text-sm text-white'>{`a personalised solutions with unmatched speed and convenience.`}</p>
+                    </div>
                 </div>
                 <div className='flex items-center justify-center mt-20'>
                     <Image src={featureList} height={200} alt='' />
