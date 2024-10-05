1117304ab4ada291b668419dd81a6215d42f1866
cysbyte <chenyunshu2@gmail.com>
2024-10-05 23:34:59 +0800

    feat: adjust styles

--- src/components/home/solutions/entrepreneur.tsx
+++ src/components/home/solutions/entrepreneur.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import check from '/public/home/check.svg'
-import icon from '/public/home/influencer-basic-icon.svg'
+import icon from '/public/home/influencer-entrepreneur-icon.svg'
 import whiteArrow from '/public/home/white-arrow.svg'
 
 const Entrepreneur = () => {
   return (
-    <div data-aos="fade-in" className='max-h-80 flex-1 bg-gradient-to-r from-[#FFAC9B] to-[#686EFF] flex flex-col items-center justify-center text-center gap-5 rounded-3xl p-4 backdrop-blur-md drop-shadow-lg'>
-      <Image className='w-[66px] h-auto backdrop-blur-md drop-shadow-sm' src={icon} alt='' />
+    <div data-aos="fade-in" className='max-h-80 flex-1 bg-gradient-to-r from-[#CD97BC] to-[#9D84DC] flex flex-col items-center justify-center text-center gap-5 rounded-3xl p-4 backdrop-blur-md drop-shadow-lg shadow-xl'>
+      <Image className='w-[66px] h-auto backdrop-blur-xl drop-shadow-sm' src={icon} alt='' />
       <div className='flex flex-col gap-3'>
         <h2 className='text-white font-bold text-2xl'>Entrepreneur</h2>
         <div className='pl-2 flex items-center gap-3 w-full'>
--- src/components/home/solutions/premium.tsx
+++ src/components/home/solutions/premium.tsx
@@ -6,9 +6,9 @@ import whiteArrow from '/public/home/white-arrow.svg'
 
 const Premium = () => {
   return (
-    <div data-aos="fade-in" className=' max-h-[25rem] flex-1 bg-gradient-to-r from-[#FFAC9B] to-[#686EFF] flex flex-col items-center justify-center text-center gap-5 rounded-3xl px-4 py-5 backdrop-blur-md drop-shadow-lg'>
-      <h1 className='px-2 text-white text-left w-full text-sm'>RECOMMENDED</h1>
-      <Image className='w-[66px] h-auto backdrop-blur-md drop-shadow-sm' src={icon} alt='' />
+    <div data-aos="fade-in" className=' max-h-[25rem] flex-1 bg-gradient-to-r from-[#9A84DE] to-[#7379FF] flex flex-col items-center justify-center text-center gap-5 rounded-3xl px-4 py-5 backdrop-blur-lg drop-shadow-xl shadow-xl'>
+      <h1 className='px-2 text-white text-left w-full text-[0.78rem]'>RECOMMENDED</h1>
+      <Image className='w-[66px] h-auto backdrop-blur-xl drop-shadow-sm' src={icon} alt='' />
       <div className='flex flex-col gap-3'>
         <h2 className='text-white font-bold text-2xl'>Influencer <br/> Premium</h2>
         <div className='pl-2 flex items-center gap-3 w-full'>
