dcfac9e6ebd0dd8b89117121786a7538e6312171
cysbyte <chenyunshu2@gmail.com>
2024-10-09 00:02:28 +0800

    fix: fixed bugs

--- src/components/home/header/menu.tsx
+++ src/components/home/header/menu.tsx
@@ -28,7 +28,7 @@ const Menu = () => {
             <div className='flex gap-5 items-center justify-center border-[2px] border-black rounded-full p-1 text-base'>
                 {
                     items.map((item) => (
-                        <div
+                        <div key={item.id}
                             onClick={() => setCurrentItemIndex(item.id)}
                             className={`cursor-pointer px-4 py-1 text-black rounded-full ${currentItemIndex === item.id ? 'bg-black text-white' : ' bg-white hover:bg-[#EFEFEF]'}`}>
                             {item.name}
